d589a30fe077afa443f83d8a5b3d88bba16b022a
Almog Ethel Shimon <almogsh@payoneer.com>
2021-08-21 17:42:11 +0300

    More crop_neck adjustments

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from utils.guitar_image import GuitarImage
 if __name__ == '__main__':
     Emaj_chord = "x,7,6,4,5,4"
 
-    for filename in filter(lambda x: 'gitkeep' not in x and not Path(x).is_file(), os.listdir(Path(r"C:\Users\almogsh\PycharmProjects\Py_ChordAR\photos\ms"))):
+    for filename in filter(lambda x: 'gitkeep' not in x and not Path(x).is_file(), os.listdir(Path(r"C:\Users\almogsh\PycharmProjects\Py_ChordAR\photos\asd"))):
         try:
             guitar = GuitarImage(
-            img_path=Path(rf"C:\Users\almogsh\PycharmProjects\Py_ChordAR\photos\ms\{filename}"))  # , file_name=filename)
-            # guitar.get_chord_coordinates(Emaj_chord)
+            img_path=Path(rf"C:\Users\almogsh\PycharmProjects\Py_ChordAR\photos\asd\{filename}"))  # , file_name=filename)
+            guitar.get_chord_coordinates(Emaj_chord)
         except Exception as e:
             print(rf"{filename} : {e}")
     # guitar = GuitarImage(img_path=Path(
