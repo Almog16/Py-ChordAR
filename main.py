473ae33b763f7a3520c3607c0a50aede6a0165fa
Almog Ethel Shimon <almogsh@payoneer.com>
2021-09-03 13:39:21 +0300

    Added get_string_line_equation_func which returns a function for the string's line equation

--- main.py
+++ main.py
@@ -8,22 +8,16 @@ from matplotlib import pyplot as plt
 from utils.guitar_image import GuitarImage
 
 if __name__ == '__main__':
-    Emaj_chord = "2,4,4,2,2,2"
+    Emaj_chord = "x,7,6,4,5,4"
 
-    for filename in filter(lambda x: 'gitkeep' not in x and not Path(x).is_file(), os.listdir(Path(r"C:\Users\Ella\Documents\Ella Studies\year3\ChordAR\images"))):
+    for filename in filter(lambda x: 'gitkeep' not in x and not Path(x).is_file(), os.listdir(Path(r"C:\Users\almogsh\PycharmProjects\Py_ChordAR\photos\asd\ella"))):
         try:
-
-            # img_path=Path(rf"C:\Users\almogsh\PycharmProjects\Py_ChordAR\photos\asd\ella\{filename}"))  # , file_name=filename)
-            # guitar.cropped.plot_img()
-            # guitar.get_chord_coordinates(Emaj_chord)
-            # guitar.plot_img()
             guitar = GuitarImage(
-            img_path=Path(rf"C:\Users\Ella\Documents\Ella Studies\year3\ChordAR\images\{filename}")) # , file_name=filename)
+            img_path=Path(rf"C:\Users\almogsh\PycharmProjects\Py_ChordAR\photos\asd\ella\{filename}"))  # , file_name=filename)
             # guitar.cropped.plot_img()
             guitar.get_chord_coordinates(Emaj_chord)
-            cv2.imshow("", cv2.cvtColor(guitar.color_img, cv2.COLOR_BGR2RGB))
-            cv2.waitKey()
-            # guitar.flipped.plot_img()
+            guitar.plot_img()
+            print(filename + " SUCCESS")
         except Exception as e:
             print(rf"{filename} : {e}")
     # guitar = GuitarImage(img_path=Path(
